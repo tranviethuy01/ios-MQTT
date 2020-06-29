4ab712406ed4860ba3c2befd92c02de96d69abd4
TRAN VIET HUY <viethuy@lozi.vn>
2020-06-29 20:05:54 +0700

    init project

---
+++ MQTT Chat/MQTT Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MQTT Chat
+//
+//  Created by Tran Viet Huy on 6/29/20.
+//  Copyright © 2020 Tran Viet Huy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
